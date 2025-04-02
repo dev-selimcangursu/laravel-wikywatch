7d3e1d2c772476409bf326dd058eb013cc7d1eab
Dev Selimcan <selimcangursu@asemsoft.com>
2025-04-02 18:02:41 +0300

    header alanını temsili yapısı oluşturuldu.

---
+++ wikywatch/resources/views/client/login.blade.php
@@ -0,0 +1,20 @@
+@extends('client.partials.master')
+@section('main')
+<section id="login__wrapper">
+    <h3 class="login__global__text">Ödeme seçeneklerini hızlandırmak için oturum açın.</h3>
+    <div class="login__area">
+         <p class="login__text">Wiky Watch'da oturum açın</p>
+        <form action="">
+            <input type="text" placeholder="E-Posta Adresi">
+            <input type="text" placeholder="Parola">
+            <button>Giriş Yap</button>
+            <input type="checkbox">
+            <a href="">Parolayı mı Unuttunuz ?</a>
+            <small>Wiky Watch Hesabınız yok mu?<a href="">Şimdi Oluşturun</a></small>
+        </form>
+    </div>
+    <div class="login__wrapper__bottom">
+        <p>Daha fazla yardıma mı ihtiyacınız var? <a href="">Hemen sohbet edin </a> veya 00800 448 829 873 numaralı telefonu arayın.</p>
+    </div>
+</section>  
+@endsection
---
+++ wikywatch/resources/views/client/partials/header.blade.php
@@ -0,0 +1,63 @@
+<header id="header" class="header">
+    {{-- Header Top :: Start --}}
+    <div class="header__top">
+        <div class="header__top__social__media__area">
+            <ul>
+                <li>
+                    <a href="" class="header__top__social__media__icon"><i class="bi bi-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="" class="header__top__social__media__icon"><i class="bi bi-youtube"></i></a>
+                </li>
+                <li>
+                    <a href="" class="header__top__social__media__icon"><i class="bi bi-twitter-x"></i></a>
+                </li>
+                <li>
+                    <a href="" class="header__top__social__media__icon"><i class="bi bi-facebook"></i></a>
+                </li>
+            </ul>
+         </div>
+        <div class="header__top__main">
+            <span class="header__top__main__text">Türkiyenin İlk Akıllı Çocuk Saati !</span>
+        </div>
+        <div class="header__top__button__area">
+            <button>Hemen Bilgi Al</button>
+        </div>
+     </div>
+     {{-- Header Top :: End --}}
+     {{-- Header Bottom :: Start --}}
+     <div class="header__bottom">
+        <div class="header__bottom__left">
+            <div class="header__bottom__left__logo">
+                <img src="{{asset('assets/images/wiky-logo3.svg')}}" alt="logo">
+            </div>
+            <div class="header__bottom__left__menu">
+                <ul>
+                    <li>
+                        <a class="header__bottom__left__menu__link" href="">Anasayfa</a>
+                    </li>
+                    <li>
+                        <a class="header__bottom__left__menu__link" href="">Akıllı Saatler</a>
+                    </li>
+                    <li>
+                        <a class="header__bottom__left__menu__link" href="">Aksesuar Mağazası</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="header__bottom__right">
+            <ul>
+                <li>
+                    <a class="header__bottom__right__menu__link" href="">Teknik Servis</a>
+                </li>
+                <li>
+                    <a class="header__bottom__right__menu__link" href="">Bilgi Bankası</a>
+                </li>
+                <li>
+                    <a class="header__bottom__right__menu__link" href="">Medyada Biz</a>
+                </li>
+            </ul>
+        </div>
+     </div>
+     {{-- Header Bottom :: End --}}
+</header>
